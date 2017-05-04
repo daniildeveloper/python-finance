3cf49d05b33a3bf54334f0c14e1700895d0aa90f
daniiltserin <daniilborowkow@yandex.ru>
2017-05-04 06:15:23 +0600

    Basic stock data manipulation

--- first.py
+++ first.py
@@ -6,10 +6,15 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
-start = dt.datetime(2000,1,1)
-end = dt.datetime(2017,5,1)
+df = pd.read_csv("tsla.csv", parse_dates=True, index_col=0)
 
-df = web.DataReader("IBM","yahoo", start, end)
-df[["Open", "High"]].plot()
+df["100ma"] = df["Adj Close"].rolling(window=10).mean()
+
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df["100ma"])
+ax2.plot(df.index, df["Volume"])
+
 plt.show()
-
